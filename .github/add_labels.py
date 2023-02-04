33e3b71de7515dd2ed76fd632cf614a91b66cd5d
RitheeshBaradwaj <ritheeshbaradwaj@gmail.com>
2023-02-04 23:02:19 +0900

    add py script to find matching labels

---
+++ .github/add_labels.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+def main():
+    # Get labels and formats from config file
+    labels = []
+    branch_formats = []
+    with open(".github/labels_config.txt") as f:
+        for line in f:
+            label, branch_format = line.strip().split()
+            labels.append(label)
+            branch_formats.append(branch_format)
+
+    # Get pull request details
+    pr_head_ref = os.getenv("GITHUB_HEAD_REF")
+    pr_title = os.getenv("GITHUB_PULL_REQUEST_TITLE")
+    pr_title = pr_title.lower()
+
+    matching_labels = ""
+    for i in range(len(labels)):
+        label = labels[i]
+        branch_format = branch_formats[i] + "/"
+
+        if pr_head_ref.startswith(branch_format) or label in pr_title:
+            matching_labels += label + " "
+
+    if len(matching_labels) > 0:
+        os['LABELS'] = matching_labels[:-2]
+        sys.exit(0)
+
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()

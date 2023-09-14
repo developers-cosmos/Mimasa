fd4e6e3e746bb116881f973f33de5a4a89e92e30
RitheeshBaradwaj <ritheeshbaradwaj@gmail.com>
2023-09-15 01:16:52 +0900

    Fix module import bug & improve setup_env script

--- tools/run_mimasa.sh
+++ tools/run_mimasa.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-python setup.py install
+run_setup=false
+run_download=false
+
+while getopts "sd" opt; do
+  case $opt in
+    s) # Set the run_setup flag when -s option is provided
+      run_setup=true
+      ;;
+    d) # Set the run_download flag when -d option is provided
+      run_download=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if $run_setup; then
+  python setup.py install
+fi
+
+if $run_download; then
+  python src/common/download_data.py
+fi
+
 python src/main.py
